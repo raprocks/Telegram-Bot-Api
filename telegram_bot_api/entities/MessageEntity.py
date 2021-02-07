55d6d75bc4e5ea9396e571ee7d6de441dc4d74e2
Rohit Patil <rahulhimesh09@gmail.com>
2021-02-07 21:54:49 +0530

    added the most needed messege entity entity lol

---
+++ telegram_bot_api/entities/MessageEntity.py
@@ -0,0 +1,12 @@
+from typing import Dict, Optional
+from .User import User
+
+
+class Dict:
+    def __init__(self, entity_data: Dict):
+        self.type: str = entity_data.get('type')
+        self.offset: int = entity_data['offset']
+        self.length: int = entity_data['length']
+        self.url: Optional[str] = entity_data.get('url')
+        self.user: Optional[User] = entity_data.get('user', None)
+        self.language: Optional[str] = entity_data.get('language')
---
+++ telegram_bot_api/entities/MessageId.py
@@ -0,0 +1,3 @@
+class MessageId:
+    def __init__(self, message_id: int):
+        self.message_id: int = message_id
