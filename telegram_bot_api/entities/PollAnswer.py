befea0f0f99025403e302cf54e029b81ffbb93ab
Rohit Patil <rahulhimesh09@gmail.com>
2021-02-12 17:50:59 +0530

    some type hint checking (2)

--- telegram_bot_api/entities/PollAnswer.py
+++ telegram_bot_api/entities/PollAnswer.py
@@ -4,6 +4,6 @@ from typing import Dict
 
 class PollAnswer:
     def __init__(self, poll_data: Dict):
-        self.poll_id = poll_data.get('poll_id', None)
+        self.poll_id: str = poll_data.get('poll_id', None)
         self.user: User = poll_data.get('user', None)
         self.option_ids: tuple[int, ...] = poll_data.get('option_ids', None)
--- telegram_bot_api/entities/PollOption.py
+++ telegram_bot_api/entities/PollOption.py
@@ -3,5 +3,5 @@ from typing import Dict
 
 class PollOption:
     def __init__(self, poll_option_data: Dict):
-        self.text: str = poll_option_data.get('text')
-        self.voter_count: int = poll_option_data.get('voter_count')
+        self.text: str = poll_option_data['text']
+        self.voter_count: int = poll_option_data['voter_count']
